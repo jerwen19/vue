2a29adb3fc7bdfd59580eff88118c94b5cfee16d
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2025-02-04 20:23:28 +0800

    fix userslist still showing even it was deleted already

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -3,17 +3,19 @@
         <div class="text-left">
             <h1 class=" bg-green-600 text-gray-50 inline-block p-2 rounded-md"> List of Username</h1>
         </div>
+        <p v-if="usersList.length === 0">No user found</p>
+        <ul class="" v-for="user in usersList" :key="user._id">
 
-        <ul class="flex justify-between items-center gap-3" v-for="user in usersList" :key="user._id">
-
-            <router-link class="flex justify-between" v-if="user && user._id"
-                :to="{ name: 'user-profile', params: { id: user._id } }">
-                {{ user.username }}
+            <li class="flex justify-between items-center">
+                <router-link v-if="user && user._id" :to="{ name: 'user-profile', params: { id: user._id } }">
+                    {{ user.username }}
 
 
-            </router-link>
-            <span @click="deleteUserHandler(user._id)"
-                class="bg-red-700 cursor-pointer inline-block p-2 rounded-full text-gray-50">X</span>
+                </router-link>
+                <span v-if="user._id" @click="deleteUserHandler(user._id)"
+                    class="bg-red-700 cursor-pointer inline-block p-2 rounded-full text-gray-50">X</span>
+            </li>
+
         </ul>
 
         <router-link class="bg-blue-600 cursor-pointer rounded-lg p-3 inline-block text-gray-50"
@@ -28,15 +30,17 @@
 import { onMounted, ref } from 'vue'
 import axiosClient from '../axiosClient'
 
-const usersList = ref([''])
+const usersList = ref([])
 const username = ref('')
 const password = ref('')
+const isError = ref(false)
 
 onMounted(async () => {
     await getUsers()
 })
 
 const getUsers = async () => {
+    isError.value = false;
     try {
         const response = await axiosClient.get('/getUsers')
         usersList.value = response.data.usersList;
@@ -49,7 +53,7 @@ const deleteUserHandler = async (id) => {
     try {
         const response = await axiosClient.delete('/deleteUser/' + id)
         console.log(response)
-        await getUsers()
+        usersList.value = usersList.value.filter(user => user._id !== id);
     } catch (err) {
         console.log(err.message)
     }
